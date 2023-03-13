7aa67955c8043d2ed6e3cb61d45a2d75538c3e98
Dániel B. Papp <dpapp@odu.edu>
2023-03-13 19:50:11 -0400

    Created initial routing pattern

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { CharacterComponent } from './character/character.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: AuthComponent },
+	{ path: 'characters', component: CharacterComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
