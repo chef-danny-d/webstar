c08b42209cb8217258c0e927027e076549e755f3
Dániel B. Papp <dpapp@odu.edu>
2023-03-14 23:08:17 -0400

    Updated API password calls

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from "../../env/environment";
 
 const AUTH_API =
 	'https://developer.webstar.hu/rest/frontend-felveteli/v2/authentication/';
@@ -8,7 +9,7 @@ const AUTH_API =
 const httpOptions = {
 	headers: new HttpHeaders({
 		'Content-Type': 'application/json',
-		'Applicant-Id': 'MR8wyPTU',
+		'Applicant-Id': environment.apiKey,
 	}),
 };
 
@@ -18,6 +19,12 @@ const httpOptions = {
 export class AuthService {
 	constructor(private http: HttpClient) {}
 
+  /**
+   * Login the user with the given credentials and return the token along with the user profile
+   * The authentication is completed by the API endpoint
+   * @param username
+   * @param password
+   */
 	login(username: string, password: string): Observable<any> {
 		return this.http.post(
 			AUTH_API,
--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Character } from '../characters/types';
 import { Observable } from 'rxjs';
+import { environment } from "../../env/environment";
 
 @Injectable({
 	providedIn: 'root',
@@ -9,13 +10,17 @@ import { Observable } from 'rxjs';
 export class HeroService {
 	constructor(private http: HttpClient) {}
 
+  /**
+   * Get the list of Star Wars characters from the API endpoint
+   *  @returns {Observable<Character[] | unknown>}
+   */
 	getCharacters(): Observable<any> {
 		return this.http.get<{ characters: Character[] }>(
 			'https://developer.webstar.hu/rest/frontend-felveteli/v2/characters/',
 			{
 				headers: {
 					'Content-Type': 'application/json',
-					'Applicant-Id': 'MR8wyPTU',
+					'Applicant-Id': environment.apiKey,
 					'Application-Authorization': `Bearer ${localStorage.getItem(
 						'token'
 					)}`,
